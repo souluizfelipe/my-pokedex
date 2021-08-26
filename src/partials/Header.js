13089479a1093d8cc51612b2cf1496b26630aeb4
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-26 08:09:17 -0300

    Create Header Structure

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -1,9 +1,41 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 
 const Header = () => {
-  return(
-    <h1>Header</h1>
-  )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Pokedex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+    );
 }
 
 export default Header
