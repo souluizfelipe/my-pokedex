a047ff5a0ceb17251962cbad01105b013bc0470f
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-08-26 08:28:50 -0300

    create Component Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-
+import CardComponent from '../components/CardComponent'
 
 const Home = () => {
   return (
     <>
       <h1>Pokedex - Home</h1>
+      <CardComponent 
+        title="hey"
+        message="this is the message on the Card"
+        image="http://github.com/souluizfelipe.png"
+      />
     </>
   )
 }
